188ce6afa5d2fc1931fc7855415ced92e4f1c96c
Lee Jia Sheng <142868124+JSLee0920@users.noreply.github.com>
2025-11-17 02:39:49 +0000

    feat: add code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,32 @@
-print("Hello World")
+import streamlit as st
+import clips
+import logging 
+
+# Setup working environment
+logging.basicConfig(level=15, format='%(message)s')
+
+env = clips.Environemnt()
+router = clips.LoggingRouter()
+env.add_router(router)
+
+
+# Input
+
+name = st.text_input("Enter your name")
+
+# Knowledge base
+env.build('(deftemplate result (slot name))')
+
+env.assert_string(f'(result (name "{name}"))')
+
+# inference
+env.run()
+
+
+# output
+results = []
+for fact in env.facts():
+    if fact.template.name == 'result':
+        results.append(fact['name'])
+
+st.write(results[0], "output")
